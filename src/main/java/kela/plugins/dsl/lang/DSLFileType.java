60fd3a94c1b09e8f3b79f4c368f32060f24c22d0
Keegan Pasqualoni <keeganpeaco@yahoo.com>
2025-06-08 14:35:18 -0600

    Basic Langugage Recognition

---
+++ src/main/java/kela/plugins/dsl/lang/DSLFileType.java
@@ -0,0 +1,41 @@
+package kela.plugins.dsl.lang;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.util.NlsSafe;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class DSLFileType extends LanguageFileType {
+
+    public static final DSLFileType INSTANCE = new DSLFileType();
+
+    private DSLFileType() {
+        super(DSL.INSTANCE);
+    }
+
+    @Override
+    public @NonNls
+    @NotNull String getName() {
+        return "dsl";
+    }
+
+    @Override
+    public @NlsContexts.Label
+    @NotNull String getDescription() {
+        return "IDE support for drools DSL files";
+    }
+
+    @Override
+    public @NlsSafe
+    @NotNull String getDefaultExtension() {
+        return ".dsl";
+    }
+
+    @Override
+    public Icon getIcon() {
+        return DSLIcon.FILE;
+    }
+}
---
+++ src/main/java/kela/plugins/dsl/lang/DSLIcon.java
@@ -0,0 +1,9 @@
+package kela.plugins.dsl.lang;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class DSLIcon {
+    public static final Icon FILE = IconLoader.getIcon("/icons/dsl.png", DSLIcon.class);
+}
