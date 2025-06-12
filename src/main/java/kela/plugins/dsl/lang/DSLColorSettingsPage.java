bc84d50066bcda913df44493310995d446b42118
Keegan Pasqualoni <keeganpeaco@yahoo.com>
2025-06-11 21:35:42 -0600

    Added syntax colors

---
+++ src/main/java/kela/plugins/dsl/lang/DSLColorSettingsPage.java
@@ -0,0 +1,65 @@
+package kela.plugins.dsl.lang;
+
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.options.colors.AttributesDescriptor;
+import com.intellij.openapi.options.colors.ColorDescriptor;
+import com.intellij.openapi.options.colors.ColorSettingsPage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Map;
+
+public class DSLColorSettingsPage implements ColorSettingsPage {
+    private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
+            new AttributesDescriptor("Keyword", DSLSyntaxHighlighter.WHEN),
+            new AttributesDescriptor("Comment", DSLSyntaxHighlighter.COMMENT),
+            new AttributesDescriptor("Variables", DSLSyntaxHighlighter.VAR)
+    };
+
+    @Override
+    public Icon getIcon() {
+        return DSLIcon.FILE;
+    }
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getHighlighter() {
+        return new DSLSyntaxHighlighter();
+    }
+
+    @NotNull
+    @Override
+    public String getDemoText() {
+        return """
+        # This is a comment
+        
+        [when] I called my friend {friendName}=$f.setName({friendName});
+        
+        [then] ([Hh]e|[Ss]he) picked up the phone=$f.setIsCallable(true);
+        """;
+    }
+
+    @Nullable
+    @Override
+    public Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
+        return null;
+    }
+
+    @Override
+    public AttributesDescriptor @NotNull [] getAttributeDescriptors() {
+        return DESCRIPTORS;
+    }
+
+    @Override
+    public ColorDescriptor @NotNull [] getColorDescriptors() {
+        return ColorDescriptor.EMPTY_ARRAY;
+    }
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return "DSL";
+    }
+}
---
+++ src/main/java/kela/plugins/dsl/lang/DSLSyntaxHighlighter.java
@@ -0,0 +1,48 @@
+package kela.plugins.dsl.lang;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import kela.plugins.dsl.psi.DSLTypes;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+public class DSLSyntaxHighlighter extends SyntaxHighlighterBase {
+    public static final TextAttributesKey WHEN =
+            createTextAttributesKey("DSL_WHEN", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey THEN =
+            createTextAttributesKey("DSL_THEN", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey VAR =
+            createTextAttributesKey("DSL_VAR", DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+    public static final TextAttributesKey COMMENT =
+            createTextAttributesKey("DSL_COMMENT", DefaultLanguageHighlighterColors.DOC_COMMENT);
+
+    private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{WHEN,THEN};
+    private static final TextAttributesKey[] VAR_KEYS = new TextAttributesKey[]{VAR};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new DSLLexerAdapter();
+    }
+
+    @Override
+    public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
+        if (tokenType.equals(DSLTypes.WHEN) || tokenType.equals(DSLTypes.THEN)) {
+            return KEYWORD_KEYS;
+        }
+        if (tokenType.equals(DSLTypes.COMMENT)) {
+            return COMMENT_KEYS;
+        }
+        if (tokenType.equals(DSLTypes.LHS_VARIABLE)) {
+            return VAR_KEYS;
+        }
+        return EMPTY_KEYS;
+    }
+}
