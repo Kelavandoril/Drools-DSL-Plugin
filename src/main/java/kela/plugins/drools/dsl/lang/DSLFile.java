572dc6246a72252b1e8cda29a306642d85af80ad
Keegan Pasqualoni <keeganpeaco@yahoo.com>
2025-09-13 20:18:22 -0600

    Added DSLFile

---
+++ src/main/java/kela/plugins/drools/dsl/lang/DSLFile.java
@@ -0,0 +1,25 @@
+package kela.plugins.drools.dsl.lang;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import kela.plugins.drools.dsl.lang.DSL;
+import kela.plugins.drools.dsl.lang.DSLFileType;
+import org.jetbrains.annotations.NotNull;
+
+public class DSLFile extends PsiFileBase {
+    public DSLFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, DSL.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return DSLFileType.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "DSL File";
+    }
+}
