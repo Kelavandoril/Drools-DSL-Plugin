f768490e9ce0f6184ddd5663a3efc55f1602e3a8
Keegan Pasqualoni <keeganpeaco@yahoo.com>
2025-09-14 11:32:39 -0600

    Syntax Highlighting (DSL)
    * Tweaked build.yml
    * Added syntax highlighting for DSL
    * Added settings page for syntax color editing

---
+++ src/main/java/kela/plugins/drools/dsl/lang/highlighter/DSLSyntaxHighlighterFactory.java
@@ -0,0 +1,15 @@
+package kela.plugins.drools.dsl.lang.highlighter;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public final class DSLSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new DSLSyntaxHighlighter();
+    }
+}
