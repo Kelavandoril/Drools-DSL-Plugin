a31702a0aa02e609082241f80dd235fde9610623
Keegan Pasqualoni <keeganpeaco@yahoo.com>
2025-10-02 20:50:07 -0600

    Added commenter (DSLR)

---
+++ src/main/java/kela/plugins/drools/dslr/lang/highlighter/DSLRCommenter.java
@@ -0,0 +1,39 @@
+package kela.plugins.drools.dslr.lang.highlighter;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DSLRCommenter implements Commenter {
+    @Override
+    public @Nullable String getLineCommentPrefix() {
+        return "//";
+    }
+
+    @Override
+    public @NotNull List<String> getLineCommentPrefixes() {
+        return List.of("//", "#");
+    }
+
+    @Override
+    public @Nullable String getBlockCommentPrefix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
